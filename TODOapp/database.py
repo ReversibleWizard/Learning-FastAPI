77c86c9f46dff8fff9061722ef23dfcb9266c8c7
Sayak Mitra Majumder <126380505+ReversibleWizard@users.noreply.github.com>
2026-02-21 18:16:51 +0530

    Update database.py

--- TODOapp/database.py
+++ TODOapp/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:sayak2004@localhost:5432/TodoApplicationDatabase'  also have to install psycopg2-binary
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:sayak2004@127.0.0.1:3306/todoapplicationdatabase' # also need to install pymysql package to use mysql
+SQLALCHEMY_DATABASE_URL = 'postgresql://deployed_todo_db_user:HxXC81a43UqdrWJtY5mNOatq3iq7olwg@dpg-d6cqh8ngi27c7386i620-a/deployed_todo_db'  # also have to install psycopg2-binary
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:sayak2004@127.0.0.1:3306/todoapplicationdatabase' # also need to install pymysql package to use mysql
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
